3b6ba469da0f6cb6d026db7a270663bfdc5d537f
xwu428 <xwu@gatech.edu>
2020-04-03 14:25:22 +0200

    add new test env

--- lib/model.py
+++ lib/model.py
@@ -9,33 +9,39 @@ class DenseFeatureExtractionModule(nn.Module):
     def __init__(self, finetune_feature_extraction=False, use_cuda=True):
         super(DenseFeatureExtractionModule, self).__init__()
         
-        model = models.vgg16(pretrained=True)
+        if 0:
+            model = models.vgg16(pretrained=True)
         
-        vgg16_layers = [
-            'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2',
-            'pool1',
-            'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2',
-            'pool2',
-            'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3', 'relu3_3',
-            'pool3',
-            'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3', 'relu4_3',
-            'pool4',
-            'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3', 'relu5_3',
-            'pool5'
-        ]
-        
-        #conv4_3_idx = vgg16_layers.index('conv4_3')
+            vgg16_layers = [
+                'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2',
+                'pool1',
+                'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2',
+                'pool2',
+                'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3', 'relu3_3',
+                'pool3',
+                'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3', 'relu4_3',
+                'pool4',
+                'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3', 'relu5_3',
+                'pool5'
+            ]
 
-        #self.model = nn.Sequential(
-        #    *list(model.features.children())[: conv4_3_idx + 1]
-        #)
+            if 0:
+                conv4_3_idx = vgg16_layers.index('conv4_3')
+                self.model = nn.Sequential(
+                    *list(model.features.children())[: conv4_3_idx + 1]
+                )
+            else:
+                conv5_3_idx = vgg16_layers.index('conv5_3')
+                self.model = nn.Sequential(
+                    *list(model.features.children())[: conv5_3_idx + 1]
+                )    
+        else:
+            model = models.resnet50(pretrained=True)
 
-        conv5_3_idx = vgg16_layers.index('conv5_3')
-
-        self.model = nn.Sequential(
-            *list(model.features.children())[: conv5_3_idx + 1]
-        ) 
-        
+            self.model = nn.Sequential(
+                *list(model.features.children())[: -2]
+            )
+            
         self.num_channels = 512
         
         # Fix forward parameters
